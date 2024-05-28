65efd69d52ddd9abbc56fa91f6f89343ab8c1aea
TrujiDev <eetgrisales@gmail.com>
2024-05-28 17:45:34 -0500

    Revert "feat: add NATS service constant"
    This reverts commit cb5a56245b66770902dfbb157ff92662bd5e5896.

--- dockerfile
+++
@@ -1,6 +0,0 @@
-FROM node:21-alpine3.19
-WORKDIR /usr/src/app
-COPY package*.json ./
-RUN npm install
-COPY . .
-EXPOSE 3004
--- src/main.ts
+++ src/main.ts
@@ -1,30 +1,8 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
-import { MicroserviceOptions, Transport } from '@nestjs/microservices';
-import { Logger, ValidationPipe } from '@nestjs/common';
-import { envs } from './config';
 
 async function bootstrap() {
-  const logger = new Logger('AuthMicroservice');
-
-  const app = await NestFactory.createMicroservice<MicroserviceOptions>(
-    AppModule,
-    {
-      transport: Transport.NATS,
-      options: {
-        servers: envs.natsServers,
-      },
-    },
-  );
-
-  app.useGlobalPipes(
-    new ValidationPipe({
-      whitelist: true,
-      forbidNonWhitelisted: true,
-    }),
-  );
-
-  await app.listen();
-  logger.log(`Auth Microservice in running on port ${envs.port}`);
+  const app = await NestFactory.create(AppModule);
+  await app.listen(3000);
 }
 bootstrap();
