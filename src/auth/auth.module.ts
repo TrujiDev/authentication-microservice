454576b239f3789f9ad7e3584f1d09cababb6340
TrujiDev <eetgrisales@gmail.com>
2024-05-30 18:11:46 -0500

    Update AuthModule with JwtModule integration
    This commit updates the AuthModule by integrating JwtModule from '@nestjs/jwt' for JWT token management. The changes include:
    
    - Import JwtModule and envs from 'src/config'
    - Add JwtModule.register to configure global JWT settings such as secret key and expiration time
    - Update the imports section to reflect the new dependencies
    
    By integrating JwtModule, the AuthModule gains enhanced functionality for managing JWT tokens, including global JWT configuration for security and token expiration.

--- src/auth/auth.module.ts
+++ src/auth/auth.module.ts
@@ -1,8 +1,17 @@
 import { Module } from '@nestjs/common';
 import { AuthController } from './auth.controller';
 import { AuthService } from './auth.service';
+import { JwtModule } from '@nestjs/jwt';
+import { envs } from 'src/config';
 
 @Module({
+  imports: [
+    JwtModule.register({
+      global: true,
+      secret: envs.jwtSecret,
+      signOptions: { expiresIn: '2h' },
+    }),
+  ],
   controllers: [AuthController],
   providers: [AuthService],
 })
